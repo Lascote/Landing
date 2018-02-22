880df8a3d24a2db3d1b683955de1bdf75b6b018c
Lascote <ilya.silitski@gmail.com>
2018-02-22 02:05:24 +0200

    media queries fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public', {index: 'index.html'}))
+app.use(express.static('public', {index: 'index.html'}));
 
 app.listen(8081, function () {
   console.log('Example app listening on port 8081!');
