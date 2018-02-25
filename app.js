3b7e2d57d86fcf1e1ae738af0c6fcf0d8fb25e16
Ilya <ilya.by@bk.ru>
2018-02-25 11:58:06 +0300

    Added a module

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var data = require('./public/js/module/data');
 
 app.use(express.static('public', {index: 'index.html'}));
 
 app.listen(8081, function () {
   console.log('Example app listening on port 8081!');
-});
+  //$("footer").text(data.myDateTime());
+})
