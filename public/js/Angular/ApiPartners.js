18b41358f090824d851d6baa10ca1606e90c2245
Ilya Dzeraziak <ilya.by@bk.ru>
2018-04-01 19:01:07 +0300

    Added an Angular Module

---
+++ public/js/Angular/ApiPartners.js
@@ -0,0 +1,9 @@
+var app = angular.module('ApiPartners', []);
+
+app.controller('Api', function($scope, $http){
+    $http.get('https://jsonplaceholder.typicode.com/users')
+    .then(function(response) {
+        $scope.partnersApi = response.data; 
+    });
+
+});
